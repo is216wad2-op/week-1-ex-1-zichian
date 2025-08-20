1bb501864f257d4a51cd9d67a067bd499bfb2784
Wong Zi Chian <wzc@Wongs-MacBook-Air.local>
2025-08-20 13:33:25 +0800

    done

--- check_sunday_selected.sh
+++ check_sunday_selected.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Checks if the Sunday option is selected in index.html
-grep -qi "<option value=['\"]Sunday['\"] selected" index.html
+grep -qi "<option value=['\"]Sunday['\"] selected" wk1exercise1.html
